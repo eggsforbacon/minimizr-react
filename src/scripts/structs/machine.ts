f2b47b8d06fb23574de4ab0eaa4ead9a0df3b613
zacwastaken <doicirohangnan@gmail.com>
2022-09-29 11:53:53 -0500

    finished Mealy and Moore machine implementation

--- src/scripts/structs/machine.ts
+++ src/scripts/structs/machine.ts
@@ -5,16 +5,14 @@ class Moore<S, R> {
     private matrix : S[][][];
     private index : Vertex<R>[];
     private size : number;
-    private ptr : number;
 
     constructor() {
         this.matrix = [];
         this.index = [];
         this.size = 0;
-        this.ptr = 0;
     }
 
-    public addVertex(name: string, output: R) : [Vertex<R>, number] {
+    addVertex(name: string, output: R) : [Vertex<R>, number] {
 
         let vertex = new Vertex(name, output);
         this.size = this.index.push(vertex);
@@ -22,7 +20,7 @@ class Moore<S, R> {
         return [vertex, this.size];
     }
 
-    public removeVertex(name: string) : [Vertex<R> | undefined, number] {
+    removeVertex(name: string) : [Vertex<R> | undefined, number] {
         let index : number = this.index.findIndex(v => v.name === name);
         let vertex : Vertex<R> = this.index.splice(index)[0];
         
@@ -40,7 +38,7 @@ class Moore<S, R> {
         let row : number = this.index.findIndex(row => row.name === from);
         let col : number = this.index.findIndex(col => col.name === from);
         let transitions : S[] = this.matrix[row][col];
-        transitions === null ? transitions = [] : transitions.push(input);
+        transitions === null ? transitions = [input] : transitions.push(input);
     }
 
     removeTransition(from: string, to: string, input: S) : [string, string, S] | undefined {
@@ -53,21 +51,16 @@ class Moore<S, R> {
         }
     }
 
-    printMachine() : string {
-        return "";
-    }
-
     next(from : Vertex<R>) : Vertex<R>[] {
         let row : S[][] = this.matrix[ this.index.indexOf(from)];
         let connections : (number | "")[] = row.map((transition, i) => transition.length === 0 || transition === null ? '' : i);
-        // conn: ['','', 2, 3, ''] --> conn: [2, 3]
         connections = connections.filter(x => typeof x === 'number');
 
         return this.index.filter(vertex => this.index.indexOf(vertex) in connections);
     }
 
     head() : Vertex<R> {
-        return this.index[this.ptr];
+        return this.index[0];
     }
 
     getMatrix() : S[][][] {
@@ -81,12 +74,10 @@ class Moore<S, R> {
     getSize() : number {
         return this.size;
     }
-    
-    
 }
 
 class Mealy<S, R> {
-    matrix : [S, R][][];
+    matrix : [S, R][][][];
     index : string[];
     size : number;
 
@@ -95,6 +86,66 @@ class Mealy<S, R> {
         this.index = [];
         this.size = 0;
     }
+
+    addVertex(name: string) : [string, number] {
+        this.size = this.index.push(name);
+        return [name, this.size];
+    }
+
+    removeVertex(name: string) : [string | undefined, number] {
+        let index : number = this.index.findIndex(n => n === name);
+        
+        this.matrix.splice(index); // Remove row asociated to vertex
+        
+        this.matrix.forEach(row => {
+            row.splice(index); // Remove column asociated to vertex
+        });
+        
+        this.size -= 1;
+        return [name, this.size];
+    }
+
+    addTransition(from: string, to: string, inoutPair: [S, R]) : void {
+        let row : number = this.index.findIndex(row => row === from);
+        let col : number = this.index.findIndex(col => col === from);
+        let transitions : [S, R][] = this.matrix[row][col];
+        transitions === null ? transitions = [inoutPair] : transitions.push(inoutPair);
+    }
+
+    removeTransition(from: string, to: string, inoutPair: [S,R]) : [string, string, [S, R]] | undefined {
+        let row : number = this.index.findIndex(row => row === from);
+        let col : number = this.index.findIndex(col => col === from);
+        let transitions : [S, R][] = this.matrix[row][col];
+        if (transitions !== null) {
+            let index : number = transitions.indexOf(inoutPair);
+            return [from, to, transitions.splice(index)[0]];
+        }
+    }
+
+    next(from : string) : string[] {
+        let row : [S, R][][] = this.matrix[this.index.indexOf(from)];
+        let connections : (number | "")[] = row.map((transition, i) => transition.length === 0 || transition === null ? '' : i);
+        connections = connections.filter(x => typeof x === 'number');
+
+        return this.index.filter(vertex => this.index.indexOf(vertex) in connections);
+    }
+
+    head() : string {
+        return this.index[0];
+    }
+
+    getMatrix() : [S, R][][][] {
+        return this.matrix;
+    }
+
+    getIndex() : string[] {
+        return this.index;
+    }
+
+    getSize() : number {
+        return this.size;
+    }
+
 }
 
 export {Moore, Mealy}
